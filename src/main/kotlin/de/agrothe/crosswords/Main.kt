1a1608a7b1358920d27ea6db5e98985cd3791a26
agrothe1 <andreas.grothe1@gmail.com>
2024-04-02 19:20:32 +0200

    gitignore

--- src/main/kotlin/de/agrothe/crosswords/Main.kt
+++ src/main/kotlin/de/agrothe/crosswords/Main.kt
@@ -36,52 +36,48 @@ fun main() {
 fun readDictFile(pConfig: DictConfig): List<Pair<String, List<String>>> {
     val negatives = File(pConfig.NEGATIVE_LIST_FILE_NAME).readLines().toSet()
 
-    return File(pConfig.DICT_FILE_NAME).useLines { lines ->
-        lines
-            .filterNot {
-                pConfig.SKIP_TEST_LINES &&
-                    it.contains(pConfig.TEST_MARKER)}
-            // strip tailing comments
-            .map {it.substringBefore(pConfig.COMMENT_CHAR)}
-            .map {it.split(';')}
-            .filterNot {it.size < 2}
-            .mapNotNull {line ->
-                line.first().trim().let {first ->
-                    if (pConfig.LEGAL_CHARS.matches(first))
-                        Pair(first, line.drop(1).map {it.trim()})
-                    else null
+    return File(pConfig.DICT_FILE_NAME).useLines{lines ->
+        pConfig.run{
+            lines
+                // strip tailing comments
+                .map{it.substringBefore(COMMENT_CHAR)}
+                .filterNot{SKIP_TEST_LINES && it.contains(TEST_MARKER)}
+                .map{it.split(';')}
+                .filterNot{it.size < 2}
+                .mapNotNull{line ->
+                    line.first().trim().let{first ->
+                        if (LEGAL_CHARS.matches(first))
+                            Pair(first, line.drop(1).map {it.trim()})
+                        else null
+                    }
                 }
-            }
-            .filter {it.second.all {it.isNotBlank()}}
-            .toList() // file will be closed hereafter
+                .filter{it.second.all{it.isNotBlank()}}
+                .toList() // file will be closed hereafter
+                .filterNot{negatives.contains(it.first)}
+                .map{keyValue ->
+                    fun String.adjustChars(): String =
+                        this.run{
+                            if (SUBST_CHARS)
+                                CHAR_SUBSTS.fold(this){acc, subst ->
+                                    acc.replace(subst.first, subst.second)
+                                }
+                            else this
+                        }
+                        .run{if(TO_UPPER_CASE) this.uppercase()
+                                else this
+                        }
+                    Pair(keyValue.first.adjustChars(), keyValue.second)
+                }
+                // put entries w/ same key into one group
+                .groupBy{it.first}
+                .map{keyValue ->
+                    Pair(keyValue.key,
+                        keyValue.value.fold(arrayListOf<String>(),
+                            {acc, elem -> acc.addAll(elem.second); acc}
+                        ))
+                }
+        }
     }
-        .filterNot {negatives.contains(it.first)}
-        .map { keyValue ->
-            fun String.adjustChars(): String =
-                this.run {
-                    if(pConfig.SUBST_CHARS)
-                        pConfig.CHAR_SUBSTS.fold (this) { acc, subst ->
-                            acc.replace(subst.first, subst.second)}
-                    else this
-                }
-                .run {if (pConfig.TO_UPPER_CASE) this.uppercase() else this}
-
-            Pair(keyValue.first.adjustChars(), keyValue.second)
-        }
-        .groupBy {it.first}
-        .map {keyValue ->
-            Pair(keyValue.key,
-                keyValue.value.fold(arrayListOf<String>(),
-                    {acc, elem -> acc.addAll(elem.second); acc}
-                ))
-        }
-        .groupBy {it.first}
-        .map {keyValue ->
-            Pair(keyValue.key,
-                keyValue.value.fold(arrayListOf<String>(),
-                    {acc, elem -> acc.addAll(elem.second); acc}
-                ))
-        }
 }
 
 
