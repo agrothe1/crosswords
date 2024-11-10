06076ca85aa35f8443647a22c36257367f64eebf
Andreas Grothe <andreas.grothe1@gmail.com>
2024-11-10 12:38:32 +0100

    'SimpleKeyboard' added

--- KreuzwortApp/app/src/main/java/de/agrothe/kreuzwortapp/MainActivity.kt
+++ KreuzwortApp/app/src/main/java/de/agrothe/kreuzwortapp/MainActivity.kt
@@ -1,7 +1,6 @@
 package de.agrothe.kreuzwortapp
 
 import android.annotation.SuppressLint
-import android.app.Activity
 import android.content.Context
 import android.content.SharedPreferences
 import android.content.res.AssetManager
